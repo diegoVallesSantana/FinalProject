b4c42eb50e0d200e1eb31e7d78b3fb7872cc14c0
Diego <diegoesteban.vallessantana@student.kuleuven.be>
2025-12-20 12:16:49 +0100

    DM doen need testing

---
+++ studentsource2025v2/datamgr.c
@@ -0,0 +1,110 @@
+/**
+* \author {Diego Vallés}
+ */
+#include "lib/dplist.h"
+#include "config.h"
+#include "sbuffer.h"
+#include "datamgr.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+// Static documentation: https://learn.microsoft.com/fr-fr/dotnet/csharp/language-reference/keywords/static
+static dplist_t *sensor_list = NULL;
+
+static void element_free(void **element) {
+    free(*element);
+    *element = NULL;
+}
+
+static datamgr_sensor_t *find_sensor(sensor_id_t id) {
+    if (sensor_list==NULL){ return NULL;}
+    int lsize = dpl_size(sensor_list);
+    for (int i = 0; i < lsize; i++) {
+        datamgr_sensor_t *sensor = dpl_get_element_at_index(sensor_list, i);
+        if (sensor && sensor->id == id) {return sensor;}
+    }
+    return NULL;
+}
+
+static void load_map(const char *map_filename) {
+    FILE *fp = fopen(map_filename, "r");
+    if (fp == NULL) {fprintf(stderr, "Error: could not open map_file\n"); exit(EXIT_FAILURE); }
+
+    if (sensor_list == NULL) {
+        sensor_list = dpl_create(NULL, element_free, NULL);
+        if (sensor_list == NULL){fprintf(stderr, "Error: sensor list null\n");exit(EXIT_FAILURE);}
+    }
+
+    uint16_t room, sensor_id;
+    while (fscanf(fp, "%hu %hu", &room, &sensor_id) == 2) {
+        datamgr_sensor_t *sensor = malloc(sizeof(datamgr_sensor_t));
+        if (sensor==NULL){fprintf(stderr, "Error: sensor list null\n");exit(EXIT_FAILURE);}
+        sensor->id = (sensor_id_t)sensor_id;
+        sensor->room = room;
+        sensor->history_count = 0;
+        sensor->history_index = 0;
+        sensor->running_avg = 0.0;
+        sensor->last_ts = 0;
+        sensor->last_zone = 0;
+
+        for (int i = 0; i < RUN_AVG_LENGTH; i++) {
+            sensor->history[i] = 0.0;
+        }
+        dpl_insert_at_index(sensor_list, sensor, dpl_size(sensor_list), false);
+    }
+    fclose(fp);
+}
+
+void *datamgr_run(void *arg) {
+    datamgr_args_t *map = arg;
+    load_map(map->map_filename);
+
+    sensor_data_t m;
+    while (1){
+        int rc = sbuffer_remove(map->buffer, &m, SBUFFER_READER_DM);
+        if (rc == SBUFFER_SUCCESS) {
+            datamgr_sensor_t *sensor = find_sensor(m.id);
+            if (sensor==NULL) {
+                // later: write_to_log_process("Received sensor data with invalid sensor node ID ...")
+                fprintf(stderr,"Received sensor data with invalid sensor node ID %u\n",(unsigned)m.id);
+                continue;
+            }
+
+            sensor->last_ts = m.ts;
+            sensor->history[sensor->history_index] = m.value;
+            sensor->history_index = (sensor->history_index + 1) % RUN_AVG_LENGTH;
+            if (sensor->history_count < RUN_AVG_LENGTH) sensor->history_count++;
+
+            if (sensor->history_count == RUN_AVG_LENGTH) {
+                double sum = 0.0;
+                for (int i = 0; i < RUN_AVG_LENGTH; i++) sum += sensor->history[i];
+                sensor->running_avg = (sensor_value_t)(sum / RUN_AVG_LENGTH);
+
+                int zone = 0;
+                if (sensor->running_avg < SET_MIN_TEMP) zone = -1;
+                else if (sensor->running_avg > SET_MAX_TEMP) zone = +1;
+
+                if (zone != sensor->last_zone) {
+                    if (zone == -1) fprintf(stderr,"Sensor node %u reports it’s too cold (avg temp = %g)\n",(unsigned)m.id, sensor->running_avg);
+                    if (zone == +1) fprintf(stderr,"Sensor node %u reports it’s too hot (avg temp = %g)\n",(unsigned)m.id, sensor->running_avg);
+                    sensor->last_zone = zone;
+                }
+            } else {
+                sensor->running_avg = 0;
+            }
+
+        } else if (rc == SBUFFER_NO_DATA) {
+            break;
+        } else {
+            break;
+        }
+    }
+
+    return NULL;
+}
+
+void datamgr_free(){
+    if (sensor_list == NULL) return;
+    dpl_free(&sensor_list, true);
+    sensor_list = NULL;
+}
---
+++ studentsource2025v2/datamgr.h
@@ -0,0 +1,42 @@
+/**
+ * \author {Diego Vallés}
+ */
+
+#ifndef DATAMGR_H_
+#define DATAMGR_H_
+
+#include <stdint.h>
+#include <time.h>
+#include "config.h"
+#include "sbuffer.h"
+
+typedef struct {
+    sensor_id_t id;
+    uint16_t room;
+
+    sensor_value_t history[RUN_AVG_LENGTH];
+    int history_count;
+    int history_index;
+
+    sensor_value_t running_avg;
+    time_t last_ts;
+
+    int last_zone;   // -1 cold, 0 normal, +1 hot (used to avoid repeated logs)
+} datamgr_sensor_t;
+
+typedef struct {
+    sbuffer_t *buffer;         // shared buffer to consume from
+    const char *map_filename;  // typically "room_sensor.map"
+} datamgr_args_t;
+
+
+void *datamgr_run(void *arg);
+
+/**
+ * This method should be called to clean up the datamgr, and to free all used memory.
+ * After this, any call to datamgr_get_room_id, datamgr_get_avg, datamgr_get_last_modified or datamgr_get_total_sensors will not return a valid result
+ */
+void datamgr_free();
+
+
+#endif  //DATAMGR_H_
